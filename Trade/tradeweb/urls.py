9fd43f5863504a99755e4b151e4350f8d34d7470
DLUTczy <44859960+DLUTczy@users.noreply.github.com>
2021-04-21 11:38:43 +0800

    学生认证页面

--- Trade/tradeweb/urls.py
+++ Trade/tradeweb/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
+    path("student", views.student, name="student"),
+    path("ajax_school", views.ajax_school),
 ]
--- Trade/tradeweb/views.py
+++ Trade/tradeweb/views.py
@@ -1,16 +1,18 @@
+import json
+
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 from pymysql import IntegrityError
 
 from tradeweb.models import *
 
 
 def index(request):
-
     return render(request, "tradeweb/index.html")
 
 
@@ -42,17 +44,26 @@ def logout_view(request):
 def register(request):
     if request.method == "POST":
         phoneID = request.POST["phoneID"]
+        if len(phoneID) != 11:
+            return render(request, "tradeweb/register.html", {
+                "message": "请输入合法长度的手机号."
+            })
         # email = request.POST["email"]
         username = str(phoneID)
+        phoneID = int(phoneID)
         # Ensure password matches confirmation
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
             return render(request, "tradeweb/register.html", {
-                "message": "Passwords must match."
+                "message": "密码不匹配"
             })
 
         # Attempt to create new user
+        if User.objects.filter(phoneID=phoneID):
+            return render(request, "tradeweb/register.html", {
+                "message": "手机号已被注册过！"
+            })
         try:
             user = User.objects.create_user(phoneID=phoneID, username=username, password=password)
             user.save()
@@ -66,3 +77,38 @@ def register(request):
         return render(request, "tradeweb/register.html")
 
 
+def student(request):
+    if request.method == "GET":
+        cities = tb_School.objects.all().distinct().order_by("area_id").values("area_id","area_name")
+        return render(request, "tradeweb/student.html", {
+            "cities": cities,
+        })
+    elif request.method == "POST":
+        name = request.POST["name"]
+        username = request.POST["username"]
+        stuID = request.POST["student_id"]
+        school_id = request.POST["school_id"]
+        school = tb_School.objects.get(school_id=school_id)
+        try:
+            student = Student.objects.create(name=name, studentID=stuID, school=school)
+            student.save()
+            user = User.objects.get(username=username)
+            user.student = student
+            user.save()
+        except IntegrityError:
+            return render(request, "tradeweb/student.html", {
+                "message": "学号已经认证."
+            })
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return HttpResponseRedirect(reverse("index"))
+
+
+@csrf_exempt
+def ajax_school(request):
+    area_id = request.GET["area_id"]
+    schools = tb_School.objects.filter(area_id=area_id).values("school_id", "school_name")
+    list = []
+    for school in schools:
+        list.append(school)
+    return JsonResponse(json.dumps(list), safe=False)
